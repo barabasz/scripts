478f6e795741f8460beca50a57ccc22fd9d65db3
Andrew <abarabasz@gmail.com>
2024-03-19 09:45:00 +0100

    using openpyxl

--- make-dirs-from-xlsx.py
+++ make-dirs-from-xlsx.py
@@ -1,14 +1,21 @@
 import os
-import XXXXX
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
 
-# creating folders from list in XLSX file using XXXX
-# assuming that folders are in "Folder" column
-
+sheet_index = 0
+folders_col_name = "Folder"
+data_file = ".\\filename.xlsx"
 root_path = os.path.dirname(os.path.realpath(__file__))
 
-datafile = ".\\filename.xls"
-records = pyexcel.iget_records(file_name=datafile)
-folders = [record['Folder'] for record in records]
+workbook = load_workbook(filename=data_file, data_only=True)
+sheetname = workbook.sheetnames[sheet_index]
+sheet = workbook[sheetname]
+headers = [cell.value for cell in next(sheet.iter_rows(min_row=1, max_row=1))]
+colum_number = [col[0].value for col in sheet.iter_cols()].index(folders_col_name) + 1
+column_letter = get_column_letter(colum_number)
+column = sheet[column_letter]
+folders = [cell.value for cell in column if cell.row != 1]
 
 for folder in folders:
     os.mkdir(os.path.join(root_path, folder))
+    print(folder)
