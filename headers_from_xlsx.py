bb28aef3084e1aefe599b679f524ede5fced406b
Andrew <abarabasz@gmail.com>
2024-03-19 12:08:13 +0100

    Create headers_from_xlsx.py

---
+++ headers_from_xlsx.py
@@ -0,0 +1,15 @@
+from openpyxl import load_workbook
+
+headers_row = 1 # row number with headers
+sheet_num = 0 # sheet index
+file_name = 'filename.xlsx'
+
+# select workbook / Excel file
+workbook = load_workbook(filename=file_name)
+# select sheet
+sheet = workbook.worksheets[sheet_num]
+# select row with headers
+row = sheet[headers_row]
+# make list with headers
+headers = [cell.value for cell in row]
+print(headers)
