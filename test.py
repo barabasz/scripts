f834b2ad37bfbbaaa9c5611a1a316c5fc0dbd7af
Andrzej Barabasz <abarabasz@gmail.com>
2024-04-06 12:41:30 +0200

    minor

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-from rich.console import Console
-console = Console()
+from beautiful_date import *
 
-console.print("Hello World!", style="bold red")
+'''beautiful-date demo'''
+
+print(Apr)
+
+d1 = 15
+y1 = 2025
+
+a = d1-Apr-y1
+print(type(a), a)
+
+b = (29/Apr/2025)[23:45]
+print(type(b), b)
+
+f = BeautifulDate(2018, 3, 25)
+print(type(f), f)
+
+c = 5
+# print(b - a)
+print(a + c*days)
+print(b - c*days)
+
